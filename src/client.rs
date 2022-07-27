20446a9b02129b5c06ea567ffd3902d143c9bf45
kozistr <kozistr@gmail.com>
2022-07-27 10:53:44 +0900

    update: remove num_users

--- src/client.rs
+++ src/client.rs
@@ -16,7 +16,6 @@ static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 #[derive(Debug, Clone)]
 struct Config {
     addr: String,
-    users: usize,
     b: usize,
     n: usize,
     timeout: u64,
@@ -24,19 +23,17 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 5 {
+        if args.len() < 4 {
             return Err("not enough arguments");
         }
 
         let addr = args[1].clone();
-        let users = args[2].parse().unwrap();
-        let b = args[3].parse().unwrap();
-        let n = args[4].parse().unwrap();
-        let timeout = args[5].parse().unwrap();
+        let b = args[2].parse().unwrap();
+        let n = args[3].parse().unwrap();
+        let timeout = args[4].parse().unwrap();
 
         Ok(Config {
             addr: addr,
-            users: users,
             b: b,
             n: n,
             timeout: timeout,
@@ -51,93 +48,71 @@ struct Metrics {
     total_secs: f32,
 }
 
-#[derive(Default)]
-struct User {
-    id: String,
-}
+async fn execute(config: &Config) -> Result<Metrics, Box<dyn Error>> {
+    let mut client = cb::inference_client::InferenceClient::connect(config.addr.clone()).await?;
 
-impl User {
-    fn new(id: String) -> Self {
-        Self { id }
+    let request = cb::PredictRequest {
+        features: vec![
+            cb::Features {
+                float_feature1: 0.55,
+                float_feature2: 0.33,
+                cat_feature1: "A".to_string(),
+                cat_feature2: "B".to_string(),
+                cat_feature3: "C".to_string(),
+            };
+            config.b
+        ],
+    };
+
+    // warm up 10 times
+    for _ in 0..10 {
+        _ = client.predict(request.clone()).await?;
     }
 
-    async fn execute(&mut self, config: &Config) -> Result<Metrics, Box<dyn Error>> {
-        let mut client =
-            cb::inference_client::InferenceClient::connect(config.addr.clone()).await?;
+    let mut report_start = Instant::now();
+    let mut total_secs = 0.0f32;
+    let mut lat = vec![0u64; config.n];
+    let mut model_lat = vec![0u64; config.n];
 
-        let request = cb::PredictRequest {
-            features: vec![
-                cb::Features {
-                    float_feature1: 0.55,
-                    float_feature2: 0.33,
-                    cat_feature1: "A".to_string(),
-                    cat_feature2: "B".to_string(),
-                    cat_feature3: "C".to_string(),
-                };
-                config.b
-            ],
-        };
+    for i in 1..config.n {
+        let start = Instant::now();
+        let response = client.predict(request.clone()).await?.into_inner();
 
-        // warm up 10 times
-        for _ in 0..10 {
-            _ = client.predict(request.clone()).await?;
+        lat[i] = start.elapsed().as_nanos() as u64;
+        model_lat[i] = response.model_latency;
+
+        if i % REPORT == 0 {
+            let secs = report_start.elapsed().as_secs_f32();
+
+            println!("----------------------------------------------------------------------------------------------------------------------------------------------");
+            log_stats(
+                "> Model",
+                i,
+                config.n,
+                secs,
+                config.timeout,
+                &model_lat,
+                i - REPORT,
+                REPORT,
+            );
+            println!("----------------------------------------------------------------------------------------------------------------------------------------------");
+
+            total_secs += secs;
+            report_start = Instant::now();
         }
-
-        let mut report_start = Instant::now();
-        let mut total_secs = 0.0f32;
-        let mut lat = vec![0u64; config.n];
-        let mut model_lat = vec![0u64; config.n];
-
-        for i in 1..config.n {
-            let start = Instant::now();
-            let response = client.predict(request.clone()).await?.into_inner();
-
-            lat[i] = start.elapsed().as_nanos() as u64;
-            model_lat[i] = response.model_latency;
-
-            if i % REPORT == 0 {
-                let secs = report_start.elapsed().as_secs_f32();
-
-                println!("--------------------------------------------------------------------------------------------------------------------------------------------------------");
-                log_stats(
-                    ">Model",
-                    i,
-                    config.n,
-                    secs,
-                    config.timeout,
-                    &model_lat,
-                    i - REPORT,
-                    REPORT,
-                );
-                log_stats(
-                    &self.id,
-                    i,
-                    config.n,
-                    secs,
-                    config.timeout,
-                    &lat,
-                    i - REPORT,
-                    REPORT,
-                );
-                println!("--------------------------------------------------------------------------------------------------------------------------------------------------------");
-
-                total_secs += secs;
-                report_start = Instant::now();
-            }
-        }
-
-        Ok(Metrics {
-            lat,
-            model_lat,
-            total_secs,
-        })
     }
+
+    Ok(Metrics {
+        lat,
+        model_lat,
+        total_secs,
+    })
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 8)]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("========================================================================================================================================================");
-    println!("Usage: cb-client http://host:port users batch_size iterations timeout_ms");
+    println!("==============================================================================================================================================");
+    println!("Usage: cb-client http://host:port batch_size iterations timeout_ms");
 
     let args: Vec<String> = env::args().collect();
 
@@ -147,40 +122,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
     });
 
     println!(
-        "Host:{} Users:{} BatchSize:{} Iterations:{} Timeout:{}",
-        config.addr, config.users, config.b, config.n, config.timeout
+        "Host:{} BatchSize:{} Iterations:{} Timeout:{}",
+        config.addr, config.b, config.n, config.timeout
     );
-    println!("========================================================================================================================================================");
+    println!("==============================================================================================================================================");
 
     let (tx, rx) = mpsc::channel::<Metrics>();
     let start = Instant::now();
 
-    for u in 0..config.users {
+    {
         let config = config.clone();
         let tx = tx.clone();
-        let id = format!("User{:0>2}", u);
 
         tokio::spawn(async move {
-            let metrics = User::new(id.clone())
-                .execute(&config)
-                .await
-                .expect("ERROR!");
-            report(&id, 1, &config, &metrics);
+            let metrics = execute(&config).await.expect("ERROR!");
+            report(&config, &metrics);
             tx.send(metrics).unwrap();
         });
     }
 
-    let mut metrics = Metrics::default();
-    for _ in 0..config.users {
-        let result = rx.recv().unwrap();
+    let result = rx.recv().unwrap();
 
-        metrics.lat.extend(result.lat.iter());
-        metrics.model_lat.extend(result.model_lat.iter());
-        metrics.total_secs = start.elapsed().as_secs_f32();
-    }
+    let mut metrics = Metrics::default();
+    metrics.lat.extend(result.lat.iter());
+    metrics.model_lat.extend(result.model_lat.iter());
+    metrics.total_secs = start.elapsed().as_secs_f32();
 
     tokio::time::sleep(Duration::from_secs(1)).await;
-    report("TEST  ", config.users, &config, &metrics);
+
+    report(&config, &metrics);
 
     Ok(())
 }
@@ -195,18 +165,13 @@ fn log_stats(
     skip: usize,
     take: usize,
 ) {
-    let mean = latencies.iter().skip(skip).take(take).sum::<u64>() / (i - skip) as u64;
-    let max = *latencies.iter().skip(skip).take(take).max().unwrap();
-    let count = latencies
-        .iter()
-        .skip(skip)
-        .take(take)
-        .collect::<Vec<&u64>>()
-        .len();
-    let timeouts = latencies
-        .iter()
-        .skip(skip)
-        .take(take)
+    let lats = latencies.iter().skip(skip).take(take);
+
+    let mean = lats.clone().sum::<u64>() / (i - skip) as u64;
+    let max = *lats.clone().max().unwrap();
+    let count = lats.clone().collect::<Vec<&u64>>().len();
+    let timeouts = lats
+        .clone()
         .filter(|x| Duration::from_nanos(**x as u64) > Duration::from_millis(timeout))
         .collect::<Vec<&u64>>()
         .len();
@@ -240,27 +205,17 @@ fn percentiles(ps: Vec<f64>, latencies: &Vec<u64>, skip: usize, take: usize) -> 
         .collect()
 }
 
-fn report(id: &str, users: usize, config: &Config, metrics: &Metrics) {
-    println!("REPORT =================================================================================================================================================");
+fn report(config: &Config, metrics: &Metrics) {
+    println!("REPORT ==================================================================================================================================");
     log_stats(
-        ">Model",
-        users * config.n,
-        users * config.n,
+        "> Model",
+        config.n,
+        config.n,
         metrics.total_secs,
         config.timeout,
         &metrics.model_lat,
         0,
-        users * config.n,
+        config.n,
     );
-    log_stats(
-        &id,
-        users * config.n,
-        users * config.n,
-        metrics.total_secs,
-        config.timeout,
-        &metrics.lat,
-        0,
-        users * config.n,
-    );
-    println!("========================================================================================================================================================");
+    println!("=========================================================================================================================================");
 }
